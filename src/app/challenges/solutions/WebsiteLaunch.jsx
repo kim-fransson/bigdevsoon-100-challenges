7b9f22aea09e75a383cb616d9ea754278d314885
Kim Fransson <kim.fransson@sigmatechnology.se>
2024-03-26 10:23:59 +0100

    SSR for background image

--- src/app/challenges/solutions/WebsiteLaunch.jsx
+++ src/app/challenges/solutions/WebsiteLaunch.jsx
@@ -1,34 +1,29 @@
-"use client";
-
 import { josefinSans } from "@/app/fonts";
-import Head from "next/head";
+
+import bg from "../../assets/background.png";
 
 export const WebsiteLaunch = () => {
   return (
-    <>
-      <Head>
-        <link rel="preload" as="image" href="/challenges/44/background.png" />
-      </Head>
-      <main
-        className={`min-h-dvh flex items-center justify-center bg-white p-4 
-      bg-[url('/challenges/44/background.png')] bg-cover bg-no-repeat ${josefinSans.className}`}
-      >
-        <div className="grid max-w-screen-lg w-full gap-8 mb-48">
-          <h1 className="text-4xl font-bold uppercase text-center tracking-widest">
-            We&apos;re launching soon
-          </h1>
-          <div
-            className="flex flex-wrap items-center gap-x-16 justify-around py-8 px-8 backdrop-blur-md
+    <main
+      style={{ backgroundImage: `url(${bg.src})` }}
+      className={`min-h-dvh flex items-center justify-center bg-white p-4 
+      bg-cover bg-no-repeat ${josefinSans.className}`}
+    >
+      <div className="grid max-w-screen-lg w-full gap-8 mb-48">
+        <h1 className="text-4xl font-bold uppercase text-center tracking-widest">
+          We&apos;re launching soon
+        </h1>
+        <div
+          className="flex flex-wrap items-center gap-x-16 justify-around py-8 px-8 backdrop-blur-md
         border backdrop-brightness-95 rounded-3xl shadow-2xl"
-          >
-            <TimeDisplay label="days" value={9} />
-            <TimeDisplay label="hours" value={17} />
-            <TimeDisplay label="minutes" value={32} />
-            <TimeDisplay label="seconds" value={54} />
-          </div>
+        >
+          <TimeDisplay label="days" value={9} />
+          <TimeDisplay label="hours" value={17} />
+          <TimeDisplay label="minutes" value={32} />
+          <TimeDisplay label="seconds" value={54} />
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
 
