f01636ad8c7efcbd127ce0ae41df57bfd4b51a2f
Kim Fransson <kim.fransson@sigmatechnology.se>
2024-04-13 19:56:18 +0200

    added titles

--- src/app/challenges/solutions/Testimonials.jsx
+++ src/app/challenges/solutions/Testimonials.jsx
@@ -8,39 +8,47 @@ export const Testimonials = () => {
     <main
       className={`min-h-dvh flex justify-center items-center bg-slate-600 p-4 ${fredoka.className}`}
     >
-      <div className="flex flex-wrap justify-center gap-4 w-full">
-        {testimonials.map((testimonial) => (
-          <div
-            key={testimonial.name}
-            className="bg-slate-800 text-slate-300 rounded-2xl shadow-2xl p-4 max-w-xs w-full
+      <div className="text-center">
+        <h2 className="text-4xl text-slate-50 font-semibold">
+          Don&apos;t take our word for it.
+        </h2>
+        <h3 className="text-4xl mb-12 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-lime-300 to-blue-300">
+          Trust our customers
+        </h3>
+        <div className="flex flex-wrap justify-center gap-4 w-full">
+          {testimonials.map((testimonial) => (
+            <div
+              key={testimonial.name}
+              className="bg-slate-800 text-slate-300 rounded-2xl shadow-2xl p-4 max-w-xs w-full
             flex flex-col gap-4"
-          >
-            <header className="flex justify-between items-center">
-              <span className="text-7xl">
-                <FormatQuoteIcon fontSize="inherit" />
-              </span>
-              <div className="bg-gradient-to-tr from-lime-300 to-blue-300 rounded-t-lg rounded-br-lg rounded-bl-[100px]">
-                <div className="pt-1 m-1 bg-slate-900 rounded-t-md rounded-br-md rounded-bl-[96px] overflow-clip">
-                  <Image
-                    width={64}
-                    height={64}
-                    alt=""
-                    src={`https://robohash.org/${testimonial.name}?set=set5`}
-                    priority
-                  />
+            >
+              <header className="flex justify-between items-center">
+                <span className="text-7xl">
+                  <FormatQuoteIcon fontSize="inherit" />
+                </span>
+                <div className="bg-gradient-to-tr from-lime-300 to-blue-300 rounded-t-lg rounded-br-lg rounded-bl-[100px]">
+                  <div className="pt-1 m-1 bg-slate-900 rounded-t-md rounded-br-md rounded-bl-[96px] overflow-clip">
+                    <Image
+                      width={64}
+                      height={64}
+                      alt=""
+                      src={`https://robohash.org/${testimonial.name}?set=set5`}
+                      priority
+                    />
+                  </div>
                 </div>
+              </header>
+              <p>{testimonial.text}</p>
+              <div className="flex flex-col text-sm mt-auto">
+                <span className="font-medium">{testimonial.name}</span>
+                <p>
+                  {testimonial.position}{" "}
+                  <span className="text-lime-300">@{testimonial.company}</span>
+                </p>
               </div>
-            </header>
-            <p>{testimonial.text}</p>
-            <div className="flex flex-col text-sm mt-auto">
-              <span className="font-medium">{testimonial.name}</span>
-              <p>
-                {testimonial.position}{" "}
-                <span className="text-lime-300">@{testimonial.company}</span>
-              </p>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </main>
   );
